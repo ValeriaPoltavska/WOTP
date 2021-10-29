8bca99a858a1f5ce16d51b8443462d814193efbb
ValeriaPoltavska <lera.poltavska@gmail.com>
2021-10-29 20:37:21 +0300

    WOTP_Lab1
    Lab1

---
+++ Laba1/Laba1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Laba1.Models;
+using Laba1.Models.Home;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laba1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            IndexVM VM = new IndexVM() { MyFullName = "Valeria"};
+            return View(VM);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Laba1/Laba1/Models/Home/IndexVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laba1.Models.Home
+{
+    public class IndexVM
+    {
+        public string MyFullName{ get; set; }
+        
+    }
+}
