9160901ba05ff460d1b75603edd493ad2d90cdd3
ValeriaPoltavska <lera.poltavska@gmail.com>
2021-12-17 17:29:50 +0200

    Lab6

---
+++ Laba1/Laba1/Views/Shared/Components/Topbar/TopbarViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Laba1.Views.Shared.Components.Topbar
+{
+    public class TopbarViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            TopbarViewModel vm = new TopbarViewModel()
+            {
+                IsAdminUser = false
+            };
+            return View(vm);
+        }
+    }
+}
+
